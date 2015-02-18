e2ebc707456ec99ee57d01eed52c67a8a6361dd2
NURSE Dana <Dana.Nurse@ricardo-aea.com>
2015-02-18 13:26:01 +0000

    Initial Commit

---
+++ ConfigPlay/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keys = ConfigurationManager.AppSettings.AllKeys;
+
+            foreach(var key in keys)
+            {
+                Console.WriteLine("{0} : {1}", key, ConfigurationManager.AppSettings.Get(key));
+                Debug.Assert(false, key);
+            }
+
+            SsrsConfig ssrsConfig =
+                (SsrsConfig)ConfigurationManager.GetSection("ssrsConfigGroup/ssrsConfig");
+
+            Console.WriteLine(ssrsConfig.Url);
+            Console.WriteLine(ssrsConfig.ServiceUrl);
+            Console.WriteLine(ssrsConfig.ReportsPath);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConfigPlay/SsrsConfigSection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigPlay
+{
+    public class SsrsConfig : ConfigurationSection
+    {
+        [ConfigurationProperty("url", DefaultValue = "", IsRequired = false)]
+        public string Url 
+        {
+            get { return (string)this["url"]; }
+            set { this["url"] = value; }
+        }
+
+        [ConfigurationProperty("serviceUrl", DefaultValue = "", IsRequired = false)]
+        public string ServiceUrl
+        {
+            get { return (string)this["serviceUrl"]; }
+            set { this["serviceUrl"] = value; }
+        }
+
+        [ConfigurationProperty("reportsPath", DefaultValue = "", IsRequired = false)]
+        public string ReportsPath
+        {
+            get { return (string)this["reportsPath"]; }
+            set { this["reportsPath"] = value; }
+        }
+    }
+
+    public class MySection : ConfigurationSection
+    {
+        [ConfigurationProperty("x", DefaultValue = false, IsRequired = false)]
+        public Boolean X
+        {
+            get
+            {
+                return (Boolean)this["x"];
+            }
+            set
+            {
+                this["x"] = value;
+            }
+        }
+    }
+}
